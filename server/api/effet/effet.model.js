56d2b1424bff304e977189855c06108ad7037e30
marc <marc.foletto@gmail.com>
2016-05-03 11:45:40 +0200

    db

--- server/api/effet/effet.model.js
+++ server/api/effet/effet.model.js
@@ -3,10 +3,26 @@
 import mongoose from 'mongoose';
 
 var EffetSchema = new mongoose.Schema({
-  name: String,
-  info: String,
+  titre: String,
+  description: String,
   type: String,
-  active: Boolean
+  disponible: Boolean,
+  options: {}
+});
+
+var OptionSchema = new mongoose.Schema({
+  titre: String,
+  description: String,
+  disponible: Boolean,
+  prix: Number,
+  media: [],
+  dimensions: {x:Number, y:Number},
+  composants: {}
+});
+
+var ComposantEffetSchema = new mongoose.Schema({
+  compo_id: [],
+  coordonnées: {x:Number, y:Number}
 });
 
 export default mongoose.model('Effet', EffetSchema);
