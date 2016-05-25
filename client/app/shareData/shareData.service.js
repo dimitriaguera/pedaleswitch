a8b6f59011400c79ced8000f9d89e501d887dac4
marc <marc.foletto@gmail.com>
2016-05-25 19:46:24 +0200

    sharedata

---
+++ client/app/shareData/shareData.service.js
@@ -0,0 +1,47 @@
+'use strict';
+
+angular.module('pedaleswitchApp')
+  .factory('shareData', function ($http) {
+    var savedata = {};
+    var bibcompo = {liste:[],data:[]};
+    // Public API here
+    return {
+      $http,
+      
+      pushdata: function(effet, option) {
+
+        savedata[option._id] = option;
+        savedata[option._id]['_ideffet'] = effet._id;
+        savedata[option._id]['type'] = effet.type;
+
+
+        // Fait un tableau de tout les composants de l'option selectionne.
+        var arr_l = option.composants.length,
+            i;
+        var newcompo = [];
+        for (i = 0; i < arr_l; ++i) {
+          newcompo = newcompo.concat(option.composants[i]['available_compo_id']);
+        }
+        
+        // Difference entre la biblio et les newcompo.
+        var diff = _.difference(newcompo, bibcompo.liste);
+
+        // Charge les compos de la db.
+        this.$http.post('/api/composants/all', diff).then(response => {
+          response.data.forEach(function(item){
+            bibcompo.liste.push(item._id);
+          });
+        bibcompo.data = bibcompo.data.concat(response.data);
+        });
+      },
+      
+      getdata: function(){
+        return savedata;
+      },
+
+      removedata: function(){
+
+      }
+      
+    };
+  });
