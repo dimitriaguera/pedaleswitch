2eac767bb9e93dd3110f2c5e313eea76f4ef069a
Dimitri <dimitri.aguera@gmail.com>
2016-09-10 00:22:30 +0200

    validation form cote client

--- client/app/addeffet/editeffet/editeffet.controller.js
+++ client/app/addeffet/editeffet/editeffet.controller.js
@@ -2,31 +2,50 @@
 (function(){
 
 class EditEffetComponent {
-  constructor($stateParams, $http) {
+  constructor($stateParams, $http, $scope) {
     this.$http = $http;
+    this.$scope = $scope;
     this.entity = $stateParams.entity || {};
     this.types = $stateParams.types || {};
     this.nouv = $stateParams.nouv;
   }
 
   addEffet() {
-    if (this.entity.titre) {
-      var data = {
-        titre: this.entity.titre,
-        description: this.entity.description,
-        type: this.entity.type,
-        options:[],
-      };
-      for(var i=0; i<this.entity.options.length; i++){
-        data.options.push(this.entity.options[i]);
-      }
-      this.$http.post('/api/effets', data);
-      this.entity.titre = '';
-      this.entity.description = '';
-      this.entity.type = '';
-      this.entity.options = [];
+
+    // On emet un broadcast attrapé par showErrors.
+    // Cela permet d'appliquer l'etat d'erreur aux champs erronnés.
+    this.$scope.$broadcast('show-errors-check-validity');
+
+    // Si une erreur est detectée dans le formulaire, on stoppe l'envoie au serveur.
+    if (this.$scope.effetForm.$invalid) {
+      return; }
+
+    // Validation côté client effectuée. On construit l'objet à envoyer au serveur.
+    var data = {
+      titre: this.entity.titre,
+      description: this.entity.description,
+      type: this.entity.type,
+      options:[],
+    };
+
+    for(var i=0; i<this.entity.options.length; i++){
+      data.options.push(this.entity.options[i]);
     }
+
+    // Envoie de l'effet à l'api serveur pour enregistrement en base de donnée.
+    this.$http.post('/api/effets', data).then(function successCallback(response) {
+      // Si succes de la requete.
+    }, function errorCallback(response) {
+      // Si requete rejetée.
+    });
+
+    // RAZ des champs.
+    this.entity.titre = '';
+    this.entity.description = '';
+    this.entity.type = '';
+    this.entity.options = [];
   }
+
   updateEffet(effet) {
     if(effet._id) {
       this.$http.put('/api/effets/' + effet._id, effet);
