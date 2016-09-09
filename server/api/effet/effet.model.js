1cff36b667e9f4ce26bbf494f7bbad6f702c95e8
Dimitri <dimitri.aguera@gmail.com>
2016-09-10 00:22:30 +0200

    validation form cote client

--- server/api/effet/effet.model.js
+++ server/api/effet/effet.model.js
@@ -3,33 +3,78 @@
 import mongoose from 'mongoose';
 
 var ComposantEffetSchema = new mongoose.Schema({
-  type: String,
-  titre: String,
-  available_compo_id: [],
+  type: {
+    type: String,
+  //  required: [true, 'Champ type du composant requis']
+  },
+  titre: {
+    type: String,
+ //   required: [true, 'Champ titre du composant requis']
+  },
+  available_compo_id: {
+    type: Array,
+ //   required: [true, 'Champ éléments autorisés requis']
+  },
   pos: {
-    x:Number,
-    y:Number
+    x:{
+      type: Number,
+  //   required: [true, 'Champ X du composant requis']
+    },
+    y:{
+      type: Number,
+  //    required: [true, 'Champ Y du composant requis']
+    }
   }
 });
 
 var OptionSchema = new mongoose.Schema({
-  titre: String,
-  description: String,
-  disponible: Boolean,
-  prix: Number,
+  titre: {
+    type: String,
+ //   required: [true, 'Champ titre de l\'option requis']
+  },
+  description: {
+    type: String,
+ //   required: [true, 'Champ description de l\'option requis']
+  },
+  disponible: {
+    type: Boolean,
+ //   required: [true, 'Champ disponible de l\'option requis']
+  },
+  prix: {
+    type: Number,
+ //   required: [true, 'Champ prix de l\'option requis']
+  },
   media: [],
   size: {
-    w:Number,
-    h:Number
+    w:{
+      type: Number,
+   //   required: [true, 'Champ largeur de l\'option requis']
+    },
+    h:{
+      type: Number,
+   //   required: [true, 'Champ hauteur de l\'option requis']
+    }
   },
   composants: [ComposantEffetSchema]
 });
 
 var EffetSchema = new mongoose.Schema({
-  type: String,
-  titre: String,
-  description: String,
-  disponible: Boolean,
+  type: {
+    type: String,
+  //  required: [true, 'Champ type d\'effet requis']
+  },
+  titre: {
+    type: String,
+  //  required: [true, 'Champ titre de l\'effet requis']
+  },
+  description: {
+    type: String,
+    required: [true, 'Champ description de l\'effet requis']
+  },
+  disponible: {
+    type: Boolean,
+  //  required: [true, 'Champ disponible de l\'effet requis']
+  },
   options: [OptionSchema]
 });
 
