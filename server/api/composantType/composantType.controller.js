d4fb9cf84cbdb147b5da6088e4c1c999d82c0849
Dimitri <dimitri.aguera@gmail.com>
2016-09-20 11:28:43 +0200

    fonction de backup termine

--- server/api/composantType/composantType.controller.js
+++ server/api/composantType/composantType.controller.js
@@ -11,6 +11,9 @@
 
 import _ from 'lodash';
 import ComposantType from './composantType.model';
+import Composant from '../composant/composant.model';
+import Effet from '../effet/effet.model';
+import backUpAll from '../../backup/backup.model';
 
 function respondWithResult(res, statusCode) {
   statusCode = statusCode || 200;
@@ -29,6 +32,59 @@ function saveUpdates(updates) {
         return updated;
       });
   };
+}
+
+function updateInEffet (req, res) {
+
+  return function (entity) {
+
+    var query = Effet.find({});
+
+    query.where('options').elemMatch(function (opt) {
+      opt.where('composants').elemMatch(function (comp) {
+        comp.where('type', entity.titre)
+
+      });
+    });
+
+    var cursor = query.cursor();
+    var titre = entity.titre;
+    var newTitre = req.body.titre;
+
+      function operate (data) {
+          var o = data.options.length;
+          for ( var j = 0; j < o; j++ ){
+              var c = data.options[j].composants.length;
+              for ( var k = 0; k < c; k++ ){
+                  var composant = data.options[j].composants[k];
+                  if (composant.type === titre) {
+                      composant.type = newTitre;
+                  }
+              }
+          }
+          return data.save().then( (val) => {
+              console.log('Mise à jour réussie de l\'effet : ' + val.titre);
+              return val;
+      });
+      }
+
+      return cursor.eachAsync(operate, function(){
+          console.log('Mise à jour des effets réussie.');
+      }).then( () => {
+              return entity;
+      });
+
+  }
+}
+
+function updateInComposant (req) {
+    return function (entity) {
+        return Composant.update({type: entity.titre}, {type: req.body.titre}, {multi: true})
+            .then( () => {
+                console.log('Composants updatés: OK');
+                return entity;
+            });
+    }
 }
 
 function removeEntity(res) {
@@ -89,6 +145,9 @@ export function update(req, res) {
   }
   return ComposantType.findById(req.params.id).exec()
     .then(handleEntityNotFound(res))
+    .then(backUpAll(['effet', 'composant', 'composantType'], 'typeCompoChange'))
+    .then(updateInEffet(req, res))
+    .then(updateInComposant(req))
     .then(saveUpdates(req.body))
     .then(respondWithResult(res))
     .catch(handleError(res));
